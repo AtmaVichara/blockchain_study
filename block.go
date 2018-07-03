cab43c11a27294fa4fd87c22e681b7a21fe1a575
Joe Jobes <jmrjobes@gmail.com>
2018-07-03 10:19:55 -0600

    Add block and funcs

--- block.go
+++ block.go
@@ -1,29 +1,31 @@
 package main
 
 import (
-  "crypto/sha256"
-  "strconv"
-  "bytes"
-  "time"
+	"time"
 )
 
+// Block represents block within the Blockchain struct
 type Block struct {
-  Timestamp int64
-  Data []byte
-  PrevHash []byte
-  Hash []byte
+	Timestamp int64
+	Data      []byte
+	PrevHash  []byte
+	Hash      []byte
+	Nonce     int
 }
 
-
-func (b *Block) setHash() {
-  timestamp := []byte(strconv.FormatInt(b.Timestamp, 10))
-  headers := bytes.Join([][]byte{b.PrevHash, b.Data, timestamp}, []byte{})
-  hash := sha256.Sum256(headers)
-  b.Hash = hash[:]
-}
-
+// NewBlock creates new Block struct
 func NewBlock(data string, prevBlockHash []byte) *Block {
-  block := &Block{time.Now().Unix, []byte(data), prevBlockHash, []byte{}}
-  block.setHash()
-  return block
+	block := &Block{time.Now().Unix(), []byte(data), prevBlockHash, []byte{}, 0}
+	pow := NewProofOfWork(block)
+	nonce, hash := pow.Run()
+
+	block.Hash = hash
+	block.Nonce = nonce
+
+	return block
+}
+
+// NewGenesisBlock creates the first (genesis) block within a Blockchain
+func NewGenesisBlock() *Block {
+	return NewBlock("Genesis Block", []byte{})
 }
